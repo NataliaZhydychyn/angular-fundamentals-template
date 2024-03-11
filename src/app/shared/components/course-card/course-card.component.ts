5a3db0dfa8e646bbf17ccc01a4c348daf0cdb5f9
Nataliia Zhydychyn <nataliia.zhydychyn@gmail.com>
2024-03-11 21:47:23 +0100

    changed course-card

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.scss']
 })
-export class CourseCardComponent {}
+export class CourseCardComponent {
+  @Input() title!: string;
+  @Input() description!: string;
+  @Input() creationDate!: Date;
+  @Input() duration!: number;
+  @Input() authors!: string[];
+  @Input() editable: boolean = false; // Default value is false
+  @Output() clickOnShow: EventEmitter<void> = new EventEmitter<void>();
+}
