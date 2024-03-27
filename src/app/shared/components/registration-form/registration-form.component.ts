a33556af49e137377ba89fec3900c90e95428a59
Nataliia Zhydychyn <nataliia.zhydychyn@gmail.com>
2024-03-27 13:05:58 +0100

    add registretion form

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,13 +1,71 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+// import { Component } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-registration-form',
+//   templateUrl: './registration-form.component.html',
+//   styleUrls: ['./registration-form.component.scss'],
+// })
+// export class RegistrationFormComponent {
+  
+//   registrationForm!: FormGroup;
+//   isSubmitted = false;
+//   // Use the names `name`, `email`, `password` for the form controls.
+//   constructor(private fb: FormBuilder) {
+//     this.buildForm();
+//  }
+
+//  buildForm(): void {
+//     this.registrationForm = this.fb.group({
+//       name: ["", [Validators.required, Validators.minLength(6)]],
+//       email: ["", [Validators.required]],
+//       password: ["", Validators.required],
+//     });
+//   }
+
+//   onSubmit(): void {
+//     this.isSubmitted = true
+//     console.log(this.registrationForm.value);
+//   }
+
+//   get name() { return this.registrationForm?.get('name'); }
+//   get email() { return this.registrationForm?.get('email'); }
+//   get password() { return this.registrationForm?.get('password'); }
+
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.scss'],
 })
-export class RegistrationFormComponent {
+export class RegistrationFormComponent implements OnInit {
   registrationForm!: FormGroup;
-  // Use the names `name`, `email`, `password` for the form controls.
-}
+  isSubmitted = false;
+  
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.registrationForm = this.fb.group({
+      name: ["", [Validators.required, Validators.minLength(6)]],
+      email: ["", [Validators.required]],
+      password: ["", Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    this.isSubmitted = true;
+    console.log(this.registrationForm.value);
+  }
+
+  get name() { return this.registrationForm?.get('name'); }
+  get email() { return this.registrationForm?.get('email'); }
+  get password() { return this.registrationForm?.get('password'); }
+}
