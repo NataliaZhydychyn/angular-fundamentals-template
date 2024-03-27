3fede598daaf809bc1ac93240513578b06b7f974
Nataliia Zhydychyn <nataliia.zhydychyn@gmail.com>
2024-03-27 12:49:46 +0100

    add pipe

--- src/app/shared/pipes/duration.pipe.ts
+++ src/app/shared/pipes/duration.pipe.ts
@@ -4,5 +4,9 @@ import { Pipe } from "@angular/core";
     name: 'duration'
 })
 export class DurationPipe {
-    // Add your code here
+    transform(value: number): string {
+        const hours: number = Math.floor(value / 60);
+        const minutes: number = value % 60;
+        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+    }
 }
